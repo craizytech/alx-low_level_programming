912a19137598f5de031cce3d4d5ddffafd445993
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-18 22:29:40 -0700

    HelloWorld!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with propper grammar, but the oucome is a piece of art,\n");
+	return (0);
+}
