2107f582f40249ddf0e8921f1e99e0d0f7bc4e78
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-18 23:24:14 -0700

    HelloWorld!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string without usinf any funcions listed in NAME sec
+ * 	of the man(3) printf or man(3) puts
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(s[i]);
+		i++;
+	}
+	return (1);
+}
