bfd245b8e3ff486eae991d19ddfcb68d7b94a6c7
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-19 03:26:20 -0700

    HelloWorld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - prints a string using puts
+ * Return: 0 
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
