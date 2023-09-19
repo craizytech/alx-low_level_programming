4a6b3555e38985abcfa825e2bdd8f4b2e794e9dd
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-18 22:42:46 -0700

    HelloWorld!

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints sizes of various variable types available in x
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)", sizeof(a));
+	printf("Size of an int: %d byte(s)", sizeof(b));
+	printf("Size of a long int: %d byte(s)", sizeof(c));
+	printf("Size of a long long int: %d byte(s)" sizeof(d));
+	printf("Size of a float: %d byte(s)", sizeof(e));
+	return (0);
+}
