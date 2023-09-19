23919771be02554b0abaf2f0fb6e32e22f1b09e3
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-19 03:37:33 -0700

    HelloWorld

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the oucome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	printf("\n");
 	return (0);
 }
