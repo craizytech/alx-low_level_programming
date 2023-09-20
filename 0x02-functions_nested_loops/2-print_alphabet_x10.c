59cc60718e9a13a7c4a83d058d6196219e679fc7
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 12:04:02 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
