2031b7c8b1ce9be8d9a065b1a60a32a9b6233f60
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 00:18:31 -0700

    Variablesandifstatements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i ++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
