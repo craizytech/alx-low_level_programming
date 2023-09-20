eae38e65606931e35578779c13809d23db219744
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 02:45:34 -0700

    ifstatements

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,17 @@ int main(void)
 	{
 		for (j = 1; j < 10; j++)
 		{
-			if ( i < j)
+			if( i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 				
-				if ( i == 8 && j == 9)
+				if( i == 8 && j == 9)
 					break;
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	return (0);
 }
