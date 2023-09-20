6e3cce3b07f91ccfa84565773aea21395bfee7d2
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 13:23:31 -0700

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two nums and returns the result
+ * @a: 1st num
+ * @b: 2nd num
+ *
+ * Return: result a+b
+ */
+int add(int a, int b)
+{
+	return (a +b);
+}
