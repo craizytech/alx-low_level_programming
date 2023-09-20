d2903c8214678d75590b02b960b437788ec70a77
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 02:42:47 -0700

    ifstatements

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of 2 digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if ( i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				
+				if ( i == 8 && j == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
