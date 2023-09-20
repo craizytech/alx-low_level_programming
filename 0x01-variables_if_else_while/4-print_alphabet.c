e300a7d6947ac117c96dd1c00a3235471f74696e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-19 23:35:33 -0700

    Variablesandifstatements

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet lowercase except letters q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		while (i != 'q' || i != e)
+			putchar(i);
+	}
+	putchar('\n');
+}
