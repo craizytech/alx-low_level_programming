3d5de7fb044df532a0b2979c47865f6da8bf68f0
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-21 07:18:11 -0700

    nestedfunctions

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers starting with 1 and 2
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 50; i++)
+	{
+		printf("1, ");
+		printf("2, ")
