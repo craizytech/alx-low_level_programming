06a0d9c894e1b3bbce6b7bf79996b9a42f3e770f
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-21 06:59:05 -0700

    nestedfunctions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the sum of all natural numbers u 1024, muls of 3, 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, sum;
+	
+	sum = 0;
+	for (i =0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	return (sum);
+}
