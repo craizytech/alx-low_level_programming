85e5fe932c4930d3f692944fba96019d6e0a446e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-20 18:09:36 -0700

    nestedloops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,6 +25,10 @@ void times_table(void)
 				k = i * j;
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
+				if (j == 9)
+					break;
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
