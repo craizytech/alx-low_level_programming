8d54e5031bbd2e21c064077e5e4654f2b004155c
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-21 06:47:08 -0700

    nestedfunctions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,28 +8,31 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 99)
 	{
-		_putchar('0');
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			_putchar(',');
-			_putchar(' ');
-			if (k <= 99)
-				_putchar(' ');
-			if (k <= 9)
-				_putchar(' ');
-
-			if (k >= 100)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
+				k = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (k <= 99)
+					_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+				}
+				else if (k <= 99 && k >= 10)
+					_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
-			else if (k <= 99 && k >= 10)
-				_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
