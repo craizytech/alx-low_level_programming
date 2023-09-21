a1bffc7954c9d94245bd8e6dda728a38cb0ee21c
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-21 02:32:43 -0700

    nested

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,3 +5,31 @@
  * @n: num
  */
 void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n == 98)
+				break;
+			printf(", ");
+			n--;
+		}
+	}
+	else
+		printf("98");
+}
