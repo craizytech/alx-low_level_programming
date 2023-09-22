5029d32ef618418078179c36efbbce4e15b4db3e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 11:37:22 -0700

    nestedloops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number 1 to 100
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
