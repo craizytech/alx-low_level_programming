486c619cb0647bf982fdfd57b56eeae29b33d7f8
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 07:44:00 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - returns result of multiplication of 2 numbers
+ * @a: ist number
+ * @b: 2nd number
+ *
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
