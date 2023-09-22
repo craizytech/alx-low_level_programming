bb8497fa436326c48d1cf4f6c3c099daca0775e3
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 08:18:44 -0700

    morefunctions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,16 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
+		{
+			if (j < 10)
+				_putchar(j + '0');
+			else if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+
 		_putchar('\n');
 	}
 	_putchar('\n');
