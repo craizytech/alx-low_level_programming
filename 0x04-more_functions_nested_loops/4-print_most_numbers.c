e58a108ed41d45097b1f9cc7dd82ed8a6453d719
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 07:56:31 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints num 0 - 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+}
