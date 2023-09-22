107fe5b4a3c6b2d61e8f5e7deade8cc44aafaae3
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 05:29:07 -0700

    nestedloops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+/**
+ * main - prints the sum of fib num of values not exceeding 4M
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num1, num2, num3, sum;
+	
+	num1 = 1;
+	num2 = 2;
+	sum = 2;
+
+	while (num2 <= 4000000)
+	{
+		num3 = num1 + num2;
+
+		if (num3 % 2 == 0)
+			sum += num3;
+		num1 = num2;
+		num2 = num3;
+	}
+	printf("%lld\n", sum);
+	return (0);
+}
