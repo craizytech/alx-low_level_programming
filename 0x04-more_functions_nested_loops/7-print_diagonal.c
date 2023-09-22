22d436bcee69950bd5f769b216c25ff014e31642
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 08:59:38 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print-diagonal - prints a diagonal line on the terminal
+ * @n: length of the line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
