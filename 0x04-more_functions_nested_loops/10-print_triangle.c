da2a83fe6ec51ac4e15ad8930d3d0271ba544c64
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-22 22:40:37 -0700

    nestedloops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,3 +5,20 @@
  * @size: size of the triangle to be printed
  */
 void print_triangle(int size)
+{
+	int i,j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
