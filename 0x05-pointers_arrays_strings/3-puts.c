bd7f499337e6fe6969eb43e71107022275f1c4c0
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 05:21:14 -0700

    ponters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - writes a function that prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i);
+		i++;
+	}
+}
+
