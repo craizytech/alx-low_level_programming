68c5b6a106c5baa9e8beed8c68f4610e40d0b15a
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 10:35:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@ void puts2(char *str)
 {
 	int i = 0;
 	int b = 0;
-	
+
 	while (*(str + i) && (*(str + b) != '\0'))
 	{
 		_putchar(*(str + i));
 		i += 2;
-		b =i - 1;
+		b = i - 1;
 	}
 	_putchar('\n');
 }
