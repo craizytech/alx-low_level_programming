c679ce720f62382325a7842d5b9307731d4961d3
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 04:53:40 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string 
+ * @s: string
  *
  * Return: length of string
  */
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*(s + i) != '\0')
+	while (*(s + i) != '\0')
 		i++;
 	return (i);
 }
