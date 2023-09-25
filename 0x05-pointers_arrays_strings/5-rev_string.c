d3b7f897c1a1f47b9b7871025d33a249b8632ec2
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 05:56:45 -0700

    ponters

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,3 +2,25 @@
 
 /**
  * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char a[];
+	int i = 0;
+	int y = 0;
+
+	while (*(s + i) != 0)
+	{
+		*(a + 0) = *(s + i);
+		i++;
+	}
+	while (i >= 0)
+	{
+		*(s + i) = *(s + 0);
+		i--;
+		y++;
+	}
+	*(s + (y + 1)) = '\0';
+}
+
