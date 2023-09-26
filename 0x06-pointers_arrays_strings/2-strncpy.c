8f80819d285df3c3b6dd1d2ec3ad1c864581eb83
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-26 10:34:12 -0700

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,21 +8,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
 	int src_len = 0;
-	int dest_len = 0;
 
-	while (src[src_len])
+	while (src[i++])
 		src_len++;
-	while ((dest_len < src_len) && (dest_len <= n))
-	{
-		dest[dest_len] = src[dest_len];
-		dest_len++;
-		src_len--;
-	}
-	while (dest_len < n)
-	{
-		dest[dest_len] = '\0';
-		dest_len++;
-	}
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
