3b2ef8b385690a307849d04f46994d76e2e3abc7
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 23:56:13 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,10 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i])
+	while (*(dest + i))
 		i++;
-	while (src[j])
+
+	while (*(src +j)
 	{
 		dest[i] = src[j];
 		j++;
