5e3223f5ad44064840a50f77fcff922ab398dd21
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-25 22:30:36 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - converts numbers in a string to integers
+ * @s: array of characters (string)
+ *
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int num = 0;
+	int sum = 0;
+
+	while (s[i])
+		i++;
+	i--;
+	while (j <= i)
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			num = (int)s[j];
+			sum = sum + num;
+		}
+		j++;
+	}
+		if ( s[j-1] == '-')
+			sum -= sum;
+}
