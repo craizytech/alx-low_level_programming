6faae1f9057b62ad61ee00ced4832f8e2a4fd22b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-27 04:56:24 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers with n elements
+ * @n: number of elemets
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
