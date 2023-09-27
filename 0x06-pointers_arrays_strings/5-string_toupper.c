6f03a976b9ab21250dd95fc07e8b66e036d63345
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-27 05:33:45 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lc letters of a string to uupercase
+ * @str: pointer to the string
+ *
+ * Return: pointer
+ */
+char *string_touppper(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+
+	return (str);
+}
