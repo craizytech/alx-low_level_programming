6f0ec7ce81de8e5e0a7e60e197be2fac7d43d6e7
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-27 05:54:09 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizess the words in a string
+ * @str: string
+ *
+ * Return: pointer to str
+ */
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while(str[index])
+	{
+		while(!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
