c953c39431a7f153d921f2d3e6351f249550f40f
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-28 02:59:57 -0700

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,15 +13,17 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int j = 0;
 	unsigned int sum = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
 				sum++;
 				break;
 			}
+			if (accept[j] == '\0')
+				break;
 		}
 	}
 	return (sum);
