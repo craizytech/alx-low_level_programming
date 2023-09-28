e11eec334844fed5d19b5ba0499631e21fbb07ca
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-28 02:33:43 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the lenght of a prefix substring.
+ * @s: string to be compared against
+ * @accept: the string to be compared against s
+ *
+ * Return: the length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int sum = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				sum++;
+		}
+	}
+	return (sum);
+}
