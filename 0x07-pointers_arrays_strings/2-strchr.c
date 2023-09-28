f02177bc0d4be51a202eea67bdb3ee060a1a6e3e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-28 00:49:55 -0700

    morepointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int i;
 
-	while(s[i] != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return ((s + i));
-		i++;
+			return (s + i);
 	}
-	return (NULL);
+
+	return ('\0');
 }
