37fddb2008ac8f09c97446add42948e496d98b0e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-28 00:18:12 -0700

    morepointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the array of strings
+ * @c: the character to be located
+ *
+ * Return: pointer to the character's location
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int n = 0;
+	unsigned int i = 0;
+
+	while (s[n])
+		n++;
+
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] == c)
+			return ((s + i));
+	}
+	return (NULL);
+}
