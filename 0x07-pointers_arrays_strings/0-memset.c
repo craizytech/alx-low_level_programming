47c3e35685e8f1466420a083a8774fdf597761b9
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-27 23:35:45 -0700

    morepointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills the memory with a constant byte
+ * @s: the memory area to be filled
+ * @b: the constant byte
+ * @n: first number of bytes of the memory
+ *
+ * Return: a pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	*(s + i) = '\0';
+
+	return (s);
+}
