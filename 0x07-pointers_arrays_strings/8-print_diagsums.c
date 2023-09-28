89d97ec2dd94d4559bb18deaf3f93bd3980f7444
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-28 05:15:02 -0700

    pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the 2 diagonals in a matrix
+ * @a: the matrix(2d or 3d )
+ * @size: size 
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int prim = 0;
+	int sec = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		prim += a[i * size + i];
+		sec += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d", prim, sec);
+}
