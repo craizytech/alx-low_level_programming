f9cf90265897010952e0f1c6104c8c40775c5743
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-29 01:41:05 -0700

    pointers

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s* == '\0')
-		_putchar('\n');
-	else
+	if (s*)
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(*s + 1);
 	}
+	else
+		_putchar('\n');
 }
