3b0d72e6238c7b5f73a279fa9d17fc9e3d3da810
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-29 03:49:41 -0700

    pointers

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string using recursion
+ * @s: string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+
