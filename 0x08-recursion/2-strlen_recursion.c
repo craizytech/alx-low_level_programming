5a1be852c1bfdf2ab318f848249c926ae3305052
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-30 08:13:20 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+		sum += 1;
+	}
+}
