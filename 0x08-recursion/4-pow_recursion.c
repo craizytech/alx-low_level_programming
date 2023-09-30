244ce15876420ed4a6a438400277f260426b257d
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-30 08:37:42 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the result of x raised to y
+ * @x: base number
+ * @y: power
+ *
+ * Return: result or -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+}
