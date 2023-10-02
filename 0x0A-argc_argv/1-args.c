ca10e3f0c6dcb6e08ba1c2d05df8bf581fb6944a
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-01 21:32:17 -0700

    argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed into the main function
+ * @argc: counts the number of arguments including the prog name 
+ * @argv: array vector containing the args passed as strings
+ *
+ * Return: 0
+ */
 int main(int argc, char __attribute__((unused)) *argv[])
 { 
 	printf("%d\n", argc - 1);
