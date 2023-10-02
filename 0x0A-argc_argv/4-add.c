d3a2e04d151c527b9e1d947124f5e12fa8870101
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-01 22:59:32 -0700

    argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,13 +17,13 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			if (atoi(argv[i]) == 0 && atoi(argv[i]) < 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
 			else if (atoi(argv[i]) > 0)
-				sum += atoi(argv[i]);
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
