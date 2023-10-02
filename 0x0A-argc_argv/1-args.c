ef9c4b446c9b6795cdb2a11683e53e69046e963a
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-01 21:33:27 -0700

    argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints the number of arguments passed into the main function
- * @argc: counts the number of arguments including the prog name 
+ * @argc: counts the number of arguments including the prog name
  * @argv: array vector containing the args passed as strings
  *
  * Return: 0
  */
 int main(int argc, char __attribute__((unused)) *argv[])
-{ 
+{
 	printf("%d\n", argc - 1);
 	return (0);
 }
