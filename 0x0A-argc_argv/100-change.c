0c55e78d399669621086093ecb041260977333d0
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-02 06:36:34 -0700

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,13 +12,12 @@ int main(int argc, char *argv[])
 {
 	int num = 0;
 
-	if (arg != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	if (argc == 2)
+	else
 	{
 		int total = atoi(argv[1]);
 
