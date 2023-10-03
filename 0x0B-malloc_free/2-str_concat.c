651279f3f29d81eb786019eeb03cda29929e405c
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-03 14:27:37 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "";
 	while (*(s1 + i))
 		i++;
 	while (*(s2 + j))
