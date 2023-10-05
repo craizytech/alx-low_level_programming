48cdc954af75386c8ea3ccc6770b2eef7c6060cb
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-05 11:23:11 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,16 +17,24 @@ char *argstostr(int ac, char **av)
 
 	for (i = 1; i < ac; i++)
 	{
-		for(j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 		len++;
 	}
 	ptr = malloc(sizeof(char) * len + 1);
 
 	if (ptr == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (i = 1; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
+			ptr[index++] = av[i][j];
+		}
+		ptr[index++] = '\n';
+	}
+	ptr[index] = '\0';
+
+	return (ptr);
+}
