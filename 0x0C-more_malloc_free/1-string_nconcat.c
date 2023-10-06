c1d519a21372b9f018b93f2f2ba6eafe44b6c084
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-06 02:35:38 -0700

    moremalloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ char string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
