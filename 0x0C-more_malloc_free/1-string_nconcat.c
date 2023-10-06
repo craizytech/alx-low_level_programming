1e1b3c3d4c2dd21f92d9bc7738bb97b4f2add797
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-06 02:31:53 -0700

    moremalloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates 2 strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * @n: first number of bytes from s2
+ *
+ * Return: pointer to the new mem location that has new string
+ */
+char string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, len = 0;
+	char *ptr;
+
+	while(s1[i])
+		i++;
+
+	ptr = malloc(i + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
