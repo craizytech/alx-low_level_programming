291c91b9853258ee31fdf96e46e66b875908f912
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-07 04:44:56 -0700

    malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include "main"
+#include <stdio.h>
+#include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
+/**
+ * find_len - Finds the length of a string
+ * @str: The string to be measured
+ *
+ * Return: the length of a string
+ */
+int find_len(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		l++;
+
+	return (len);
+}
+
+/
