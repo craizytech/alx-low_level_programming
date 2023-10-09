c6be9c6f2a208717ee3474d692a40bd3fd727824
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-09 08:44:08 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the name of the file it was compiled from
  */
-void main(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
 }
