b063dc77dfcfd0fcd71510dc6c00da14c3df1022
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-11 10:28:05 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /**
- * dog_t - contains info about a dog using typedef
+ * struct dog_t - contains info about a dog using typedef
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the dog owner
