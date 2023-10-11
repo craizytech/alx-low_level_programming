42da6705d05381bdc069a12e629792c139276227
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-11 09:42:30 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name != NULL) ? d->name : "nil");
-		printf("Age: %f\n", d->age);
+		printf("Age: %f\n", (d->age) > 0 ? d->age : "nill");
 		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "nil");
 	}
 }
