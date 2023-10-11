023ec2403b34f30ffc80b8fb51540dba036c654b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-11 08:15:17 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - contains information about a dog
+ * @name: pointer to name
+ * @age: age of the dog
+ * @owner: name of the dogs owner
+ *
+ * Description: contains the description of a dog
+ */
 struct dog
 {
 	char *name;
