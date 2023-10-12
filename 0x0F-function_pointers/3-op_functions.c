f7721ea8aa4897bc9b232eaa65143e4ad2f10144
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-12 12:08:08 -0700

    function_pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,24 @@
+#include "calc.h"
+
+/**
+ * op_add - adds 2 numbers
+ * @a: no 1
+ * @b: no 2
+ * Return: result of addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds the difference between 2 numbers
+ * @a: no 1
+ * @b: no 2
+ * Return: result of subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
