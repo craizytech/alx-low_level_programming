f561d91d00885c38cfef201be8d9c8365f1da9fe
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-12 04:41:46 -0700

    function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef main_h
-#define main_h
+#ifndef function_pointers_h
+#define function_pointers_h
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 #endif
