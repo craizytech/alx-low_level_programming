015a41c483824ee126fd9bee803039deb4eaff26
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-12 10:21:42 -0700

    function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
-
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 	if (array == NULL || action == NULL)
 		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
 }
