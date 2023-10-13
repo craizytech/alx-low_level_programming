ff454a1c6255341dcda94f79bae63193db023655
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-13 01:14:18 -0700

    function_pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "calc.h"
+
+/**
+ * get_op_func - selects correct function to perform users operation
+ * @s: s is the operator passed as an argument to the program
+ * @a: first arg
+ * @b: 2nd arg
+ * Return: pointer to the func that corresponds to the operator else NULL
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_div},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	if (s == NULL)
+		return (NULL)
+
+	while (ops[i].op != NULL && ops[i].op != *s)
+		i++;
+
+	return (ops[i].f);
+}
