dfa86fb483db9cf72edb7fb85c3b773f694992b8
Eammon <craizytech@gmail.com>
2023-10-13 15:06:32 -0700

    variadic_pointers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, total = 0;
+	va_list nums;
+	unsigned int index, sum = 0;
 
-	if (n == 0)
-		return (0);
+	va_start(nums, n);
 
-	va_list args;
-	va_start(args, n);
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+	va_end(nums);
 
-	for (i = 0; i < n; i++)
-	{
-		total += va_arg(args, int);
-	}
-	va_end(args);
-	return (total);
+	return (sum);
 }
