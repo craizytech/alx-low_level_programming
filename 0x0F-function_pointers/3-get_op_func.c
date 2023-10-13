fc106b101f2b16d115aa8bf227277017a4f5c710
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-13 02:04:35 -0700

    function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,10 +20,7 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	if (s == NULL)
-		return (NULL);
-
-	while (ops[i].op != NULL && ops[i].op != *s)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
 
 	return (ops[i].f);
