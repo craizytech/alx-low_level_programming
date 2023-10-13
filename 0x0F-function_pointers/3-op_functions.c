3039c450f2268fa0851130b0dcfab0a132871d24
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-10-13 00:23:04 -0700

    function_pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -22,3 +22,32 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 
+/**
+ * op_mul - product
+ * @a: no 1
+ * @b: no 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: no 1
+ * @b: no 2
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of a number
+ * @a: no 1
+ * @b: no 2
+ * Return: result of division
+ */
+int op_mod(int a
