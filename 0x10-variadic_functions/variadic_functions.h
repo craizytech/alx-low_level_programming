4b805646b41060be21b7b97ce054d8e108077b88
Eammon <craizytech@gmail.com>
2023-10-13 17:59:49 -0700

    variadic_pointers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,8 @@
 #define VARIADIC_FUNCTION_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
