e512699a9d0649679956eb7edd5b91f59787346f
Eammon <craizytech@gmail.com>
2023-10-13 18:41:01 -0700

    variadic_pointers

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	unsigned int i;
-	char *str;
+	const char *str;
 
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char);
+		str = va_arg(strings,const char *);
 
 		printf("%s", str);
 		if (i != (n -1) && separator != NULL)
