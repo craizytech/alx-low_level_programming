7fa86fcd3185dfb43f8b480c878f46a59f1dcfd1
craizytech <craizytech@gmail.com>
2023-10-27 19:22:02 +0300

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0, index, factor, sum = 1;
+
+	if (b == NULL)
+		return (NULL);
+
+	while(*b)
+		len++;
+
+	for (index = 0; index < len - 1; index++)
+	{
+		if (b[index] != 0 || b[index] != 1)
+			return (NULL);
+		if (b[index] == '1')
+		{
+			for (factor = index; factor > 0; factor--)
+				sum *= 2;
+		}
+	}
+	return (sum);
+}
