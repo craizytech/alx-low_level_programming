ba318fddbab98f74b0b451147188eaf0bec73654
craizytech <craizytech@gmail.com>
2023-10-27 19:34:59 +0300

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,18 +2,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, index, factor, sum = 1;
+	int len = 0, index, factor;
+	unsigned int sum = 1;
 
 	if (b == NULL)
-		return (NULL);
+		return (0);
 
 	while(*b)
 		len++;
 
-	for (index = 0; index < len - 1; index++)
+	for (index = 0; index < len -1; index++)
 	{
 		if (b[index] != '0' || b[index] != '1')
-			return (NULL);
+			return (0);
 		if (b[index] == '1')
 		{
 			for (factor = index; factor > 0; factor--)
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 
 #endif
