85a9cf8dec5e582b675d81f0ea4f8da17a3901ca
craizytech <craizytech@gmail.com>
2023-10-28 15:55:44 +0300

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_binary  - prints the binary representation of a number
+ * @n: the long unsigned int that is to be printed in binary form
+ */
+void print_binary(long unsigned int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
