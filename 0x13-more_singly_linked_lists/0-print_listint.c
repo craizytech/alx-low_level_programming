6f7432766f552a1fd4af85e273db94ed40418b2b
craizytech <craizytech@gmail.com>
2023-10-31 13:57:57 +0300

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,24 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - prints each node in a list
- * @h: pointer to the head oh the list passed
- * Return: The number of nodes
+ * print_listint - prints all the elements of the list
+ * @h: pointer to the head node
+ *
+ * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
+	listint_t *ptr;
+	int count  = 0;
+	
+	ptr = h;
+	while (ptr)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		count++;
 	}
-
-	return (nodes);
+	return (count);
 }
+
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-singly linked lists
+more and more data structures
