d344c056d350ebc156c76cad9ccb1dd581e48955
craizytech <craizytech@gmail.com>
2023-11-01 19:01:45 +0300

    linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,16 +10,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int c;
-	listint_t *ptr;
 
-	ptr = head;
-
-	for (c = 0; c < index - 1; c++)
+	for (c = 0; c < index; c++)
 	{
-		ptr = ptr->next;
-
-		if (!(ptr->next))
+		if (head == NULL)
 			return (NULL);
+
+		head = head->next;
 	}
-	return (ptr->next);
+	return (head);
 }
