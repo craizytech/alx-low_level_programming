8eff621f3d5215a8d5d59e11537089db3cd1b18c
craizytech <craizytech@gmail.com>
2023-11-01 11:08:33 +0300

    linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to a head node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while(*head)
+	{
+		ptr = *head;
+		*head = *head->next;
+		free(ptr);
+	}
+	*head == NULL;
+}
