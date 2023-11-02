3f2a1f81dae0fa8b78fb84aa52d8bd7f4f9fdb6e
craizytech <craizytech@gmail.com>
2023-11-02 14:02:15 +0300

    linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	/* set the next pointer of the new node to the next element */
-	new->next = tmp->next;
+	node->next = tmp->next;
 	tmp->next = node;
 
 	return (node);
