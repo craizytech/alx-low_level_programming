70a715ad339c8bb108d9e1933d2794701e202aac
craizytech <craizytech@gmail.com>
2023-11-02 12:04:08 +0300

    linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,14 +10,23 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	/* create a tmp pointer to be used to traverse the list and a new node */
 	unsigned int count;
 	listint_t *tmp, *node;
+
+	/* assign memory to the the new node and insert the data */
+	node = malloc(sizeof(listint_t));
 	tmp = *head;
 	node->n = n;
 
-	for (count = 0; count < index; count++)
+	/* using a for loop loop through the list until the idx-1 element */
+	for (count = 0; count < idx; count++)
 		tmp = tmp->next;
+
+	/* set the next pointer of the new node to the next element */
 	node->next = tmp;
+
+	/* set the next pointer of the idx-1 element to the new node */
 	tmp = node;
 
 	return (node);
