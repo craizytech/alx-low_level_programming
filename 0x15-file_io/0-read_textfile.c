27975d8bc106a33dbd31ef6f73f0f90255555967
craizytech <craizytech@gmail.com>
2023-11-12 00:26:32 +0300

    File input and output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,6 +30,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* read the contents of the file to a tmp buffer */
 	output = read(fd, buffer, letters);
 
+	if (output == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	/* write to starndard output */
 	num = write(1, buffer, output);
 	if (num == -1)
