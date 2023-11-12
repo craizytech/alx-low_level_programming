a73809ef91e3cbd812e7ca869562b1cafd5f061c
craizytech <craizytech@gmail.com>
2023-11-12 21:34:51 +0300

    File input / output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+void close_file(int fd);
 /**
  * main - copies the content of 1 file to another
  * @argc: argument counter
@@ -7,7 +8,6 @@
  *
  * Return: 0
  */
-void close_file(int fd);
 int main(int argc, char *argv[])
 {
 	int file1, file2, w, r;
@@ -24,23 +24,24 @@ int main(int argc, char *argv[])
 	r = read(file1, buffer, 1024);
 
 	do {
-			if (file1 == -1 || r == -1)
-			{
-				dprintf(STDERR_FILENO,
-						"Error: Can't read from file %s\n", argv[1]);
-				exit(98);
-			}
-			
-			w = write(file2, buffer, r);
+		if (file1 == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n",
+					argv[1]);
+			exit(98);
+		}
 
-			if (file2 == -1 || w == -1)
-			{
-				dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			r = read(file1, buffer, 1024);
-			file2 = open(argv[2], O_WRONLY | O_APPEND);
+		w = write(file2, buffer, r);
+
+		if (file2 == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		r = read(file1, buffer, 1024);
+		file2 = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 
 	close_file(file1);
@@ -48,7 +49,10 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
+/**
+ * close_file - closes an open file
+ * @fd: file descriptor of the open file
+ */
 void close_file(int fd)
 {
 	int c;
