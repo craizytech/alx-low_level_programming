47de4ccd7e4cc70bd8044521d89fbc2a37679f07
craizytech <craizytech@gmail.com>
2023-11-12 17:07:51 +0300

    File input / output

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(o);
 		return (1);
 	}
-	w = write(o, text_content, sizeof(text_content) + 1);
+	w = write(o, text_content, (sizeof(text_content) + 1));
 	if (w == -1)
 		return (-1);
 
