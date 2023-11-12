df1ea3c01b94a0abfd7badfd48ead7fac66c554c
craizytech <craizytech@gmail.com>
2023-11-12 15:58:08 +0300

    File input / output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t o, w;
-	char *buffer;
 
 	if (filename == NULL)
 		return (-1);
 
-	int o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (o == -1)
 		return (-1);
