cb42607be81b4e5d7fbf5ec3353c5b6d16328b33
craizytech <craizytech@gmail.com>
2023-11-12 20:08:00 +0300

    File input / output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,41 +10,38 @@
 int main(int argc, char *argv[])
 {
 	int file1, file2;
-	char *buffer;
-
-	
+	char buffer[1024];
 
 	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file2 = open(arv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	w = (file2, file1);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	w = (file2, file1, read(file1, buffer, 1024));
 
 	if (file2 == -1 || w == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
 	if (close(file1) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd %d", file1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file1);
 		exit(100);
 	}
 	if (close(file2) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd %d", file2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file2);
 		exit(100);
 	}
 	return (0);
 }
-
