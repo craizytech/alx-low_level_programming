3ed621ed8ce5e6e53269d92111f8a6e56a09ae0d
craizytech <craizytech@gmail.com>
2023-11-12 15:55:03 +0300

    File input / output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - funtion that creates a file
+ * @filename: the name of the file to be created
+ * @text_content: the contents to be inserted into the new file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+
+	int o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (o == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		w = write(o, text_content, strlen(text_content));
+
+		if (w == -1)
+		{
+			close(o);
+			return (-1);
+		}
+	}
+
+	close(o);
+
+	return (1);
+}
