3d7f21225cde8bd49dd977c2d63d50b7c59c185d
craizytech <craizytech@gmail.com>
2023-11-12 17:02:21 +0300

    File input / output

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_APPEND | O_WRONLY, 0644);
+	o = open(filename, O_APPEND | O_WRONLY);
 	if (o == -1)
 		return (-1);
 	if (o != -1 && text_content == NULL)
