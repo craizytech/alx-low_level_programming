a486e4a9702aa97672b5c552e15f15123e3d98e7
craizytech <craizytech@gmail.com>
2023-11-12 13:40:33 +0300

    File input / output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,34 @@
 #include "main.h"
 
 /**
- * read_textfile - func reads the txtfile and prints it to the POSIX stdout
- * @filename: file to read from
+ * read_textfile - reads a text file and prints it to the starndard output
+ * @filename: pointer to the file
  * @letters: number of letters it should read and print
  *
- * Return: no of letters it could read and print or
- * 0 if filename is null
- * 0 if write fails or does not write the expected number of bytes
+ * Return: number of letters it could print, 0 otherwise
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
 	char *buffer;
 
-	/* check to ensure filename is not null */
 	if (filename == NULL)
 		return (0);
-	/* allocate memory to the buffer */
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	w = write(1, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	free(buffer);
-	close(o);
 
+	free(buffer);
 	return (w);
 }
