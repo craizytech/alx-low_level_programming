3e4bd0a45358ffa08f9606ac0d9e4438ac53499b
craizytech <craizytech@gmail.com>
2023-12-16 13:00:56 +0300

    singly_linked_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,9 @@ size_t print_list(const list_t *h)
 	while (ptr != NULL)
 	{
 		if (ptr->str != NULL)
-			printf("[%d] %s", ptr->len, ptr->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		else
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		count++;
 		ptr = ptr->next;
 	}
