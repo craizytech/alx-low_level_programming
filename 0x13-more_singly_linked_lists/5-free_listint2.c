6a5734e5a9d87b511c64eab6bb235bae438c2b71
craizytech <craizytech@gmail.com>
2023-12-17 03:54:39 +0300

    singly_linked_list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: double pointer to the head element of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
