324c994e914316d11f94c2fa579d5893e5996281
craizytech <craizytech@gmail.com>
2023-12-20 19:20:37 +0300

    singly_linked_list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns all the data(n) of a listint_t list
+ * @head: pointer to the head element of the list
+ *
+ * Return: sum of the list otherwise 0 if it failes
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum = 0;
+
+	if (ptr == NULL)
+		return (0);
+
+	do {
+		sum += ptr->n;
+		ptr = ptr->next;
+	} while ((ptr->next) != NULL);
+
+	return (sum);
+}
