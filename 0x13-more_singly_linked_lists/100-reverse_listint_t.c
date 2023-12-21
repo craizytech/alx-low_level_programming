2651d2fc69d8942fae691cf3d5bdb1f1f73ed3c1
craizytech <craizytech@gmail.com>
2023-12-21 20:53:35 +0300

    singly_linked_list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint_t.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: double pointer to the head element
+ * 
+ * Return: pointer to the first node of the reversed list
+ */
