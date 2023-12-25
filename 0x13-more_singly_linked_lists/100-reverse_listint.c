28b288594d5bd1a15788e0689eadf04431be7cd2
craizytech <craizytech@gmail.com>
2023-12-25 20:16:43 +0300

    singly_linked_list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,11 +11,16 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *current = *head;
 	listint_t *next = NULL, *prev = NULL;
 
+	/* Traverse and Reverse */
 	while (current != NULL)
 	{
+		/* save the current node */
 		next = current->next;
+
+		/* reverse the list */
 		current->next = prev;
 
+		/* move one step forward */
 		prev = current;
 		current = next;
 	}
