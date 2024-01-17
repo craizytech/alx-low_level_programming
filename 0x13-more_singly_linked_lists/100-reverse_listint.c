7f72cca007069d15f605d6f0b4943d844f4691f2
craizytech <craizytech@gmail.com>
2024-01-17 20:11:29 +0300

    more_linked_lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Function that reverses a linked list
+ * @head: double pointer to the beggining of the linked list
+ *
+ * Return: a pointer to the first node of the reversed list
+ */
+
