ed0381af4ddcbc537f8da6f54414ad461785d5b8
craizytech <craizytech@gmail.com>
2024-01-17 09:20:04 +0300

    linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->str = strlen(str);
+	new_node->len = strlen(str);
 	new_node->next = *head;
 
 	*head = new_node;
