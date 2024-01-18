ec71fad1898e0336d4c349676ce92a9aeb472313
craizytech <craizytech@gmail.com>
2024-01-18 15:23:45 +0300

    doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	
+
 	while (idx > 0 && current != NULL)
 	{
 		current = current->next;
