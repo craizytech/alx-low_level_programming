3bb6db9238aed238d898b145077f1fa51fc50b23
craizytech <craizytech@gmail.com>
2024-01-18 10:04:34 +0300

    reversed a linked list successfully

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,4 +6,20 @@
  *
  * Return: a pointer to the first node of the reversed list
  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *current = *head, *next = NULL;
 
+	if (head == NULL)
+		return (NULL);
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
