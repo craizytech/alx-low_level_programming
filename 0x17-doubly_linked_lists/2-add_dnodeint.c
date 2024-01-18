2dfce6c9139922447e74425c00ec244d8c3db029
craizytech <craizytech@gmail.com>
2024-01-18 13:13:24 +0300

    fixed a bug

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -33,8 +33,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->n = n;
 		new_node->prev = NULL;
 		new_node->next = *head;
+		(*head)->prev = new_node;
 		*head = new_node;
 		return (new_node);
 	}
 }
-
