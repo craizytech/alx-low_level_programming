8242f823d14f0369bf8535032c45442b6375fb29
craizytech <craizytech@gmail.com>
2024-01-18 09:39:44 +0300

    commiting locally since push is failing me

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Function that deletes the node at index of a list
- * @head: double pointer to the beginning of the list
- * @index: the index to be removed
+ * delete_nodeint_at_index - Function deletes a node at an index of a list
+ * @head: double pointer to the head element of a list
+ * @index: index of the node that should be deleted
  *
- * Return: 1 if succesfull 0 otherwise
+ * Return: 1 if operation succeds -1 otherwise
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current = *head, *prev = NULL;
 
 	if (head == NULL)
-		return (-1);
+		return (NULL);
+
 	if (index == 0 && current != NULL)
 	{
 		*head = (*head)->next;
@@ -26,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 		index--;
 	}
+
 	if (index > 0 || current == NULL)
 		return (-1);
 
